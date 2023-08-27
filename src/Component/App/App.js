a53034db0949313bc5cb434debf82ab9289d4baa
barook10 <madmanozil10@gmail.com>
2023-08-27 19:33:50 +0300

    starting of the application

---
+++ src/Component/App/App.js
@@ -0,0 +1,15 @@
+// src/App.js
+import React from 'react';
+import ProductList from '../ProductList/ProductList';
+import './App.css'; // Import your CSS file here
+
+function App() {
+  return (
+    <div className="App">
+      <h1>E-commerce Store</h1>
+      <ProductList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/utils/store.js
@@ -0,0 +1,13 @@
+// src/services/api.js
+const API_URL = 'https://fakestoreapi.com';
+
+export async function fetchProducts() {
+  try {
+    const response = await fetch(`${API_URL}/products`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    throw error;
+  }
+}
